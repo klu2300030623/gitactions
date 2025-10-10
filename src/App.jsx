3c0b11d10ec57745a288376e77335f043ae395a9
Pujitha <pujithashaik93@gmail.com>
2025-10-10 19:51:03 +0530

    Updated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <div classname = "container">
+  <h1>Welcome to GitActions</h1>
+  <p>This is sample home page using gitActions</p>
+  </div>
+  )
+}
+
+export default App
