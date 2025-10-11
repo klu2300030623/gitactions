a410990b515cb3d0217a1067405cc14f2e1ac9ac
Pujitha <pujithashaik93@gmail.com>
2025-10-11 20:07:42 +0530

    Updated App.js

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <div classname = "container">
   <h1>Welcome to GitActions</h1>
   <p>This is sample home page using gitActions</p>
+  <p>This is FrontEnd React app using Jenkins</p>
   </div>
   )
 }
